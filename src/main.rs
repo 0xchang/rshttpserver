0c87374910350cf0423854b24eb35df7113a4178
0xchang <oxcahng@163.com>
2023-09-16 14:42:11 +0800

    src

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,64 @@
+use clap::{value_parser, Arg, ArgMatches, Command};
 use salvo::prelude::*;
 use salvo::serve_static::StaticDir;
-use clap::{Arg,Command, ArgMatches,value_parser};
-
-
 
 #[tokio::main]
 async fn main() {
-
     let cmd = getcmd();
 
-    let port = match cmd.get_one::<u16>("port"){
+    let port = match cmd.get_one::<u16>("port") {
         Some(r) => r,
-        None => panic!("1")
+        None => panic!("1"),
     };
 
-    let ipaddr = match cmd.get_one::<String>("ipaddr"){
+    let ipaddr = match cmd.get_one::<String>("ipaddr") {
         Some(r) => r,
-        None => "0.0.0.0"
+        None => "0.0.0.0",
     };
 
-    let root = match  cmd.get_one::<String>("root"){
+    let root = match cmd.get_one::<String>("root") {
         Some(r) => r,
-        None => "."
+        None => ".",
     };
-
-
 
     tracing_subscriber::fmt().init();
 
-    let router = Router::with_path("<**path>").get(
-        StaticDir::new([
-            root
-        ])
-        .defaults("index.html")
-        .listing(true),
-    );
+    let router = Router::with_path("<**path>")
+        .get(StaticDir::new([root]).defaults("index.html").listing(true));
 
-    let acceptor = TcpListener::new(format!("{}:{}",ipaddr,port)).bind().await;
+    let acceptor = TcpListener::new(format!("{}:{}", ipaddr, port))
+        .bind()
+        .await;
     Server::new(acceptor).serve(router).await;
 }
 
-fn getcmd() -> ArgMatches{
-
+fn getcmd() -> ArgMatches {
     let matches = Command::new("rshttpserver")
-    .version("1.0")
-    .author("Your Name")
-    .about("A simple command line program")
-    .arg(
-        Arg::new("port").short('p').long("port").default_value("8075").help("set port").value_parser(value_parser!(u16))
-    ).arg(Arg::new("ipaddr").short('i').long("listen").default_value("0.0.0.0").help("set listen"))
-    .arg(Arg::new("root").short('r').long("root").default_value(".").help("set root path"))
-    .get_matches();
+        .version("1.0")
+        .author("Your Name")
+        .about("A simple command line program")
+        .arg(
+            Arg::new("port")
+                .short('p')
+                .long("port")
+                .default_value("8075")
+                .help("set port")
+                .value_parser(value_parser!(u16)),
+        )
+        .arg(
+            Arg::new("ipaddr")
+                .short('i')
+                .long("listen")
+                .default_value("0.0.0.0")
+                .help("set listen"),
+        )
+        .arg(
+            Arg::new("root")
+                .short('r')
+                .long("root")
+                .default_value(".")
+                .help("set root path"),
+        )
+        .get_matches();
     matches
-
 }
